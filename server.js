f8f0eb9d7a8aa94ddf7a849321361b5b14cddf92
Ernest Wong <ewong2222@hotmail.com>
2015-05-14 15:07:41 -0400

    use jade template

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 var express = require("express"),
+    jade = require('jade'),
     app = express();
 
 app.use("/js", express.static(__dirname + "/js"));
 app.use("/css", express.static(__dirname + "/css"));
 
+app.set('views', './views');
+app.set('view engine', 'jade');
+
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/home.html");
+  //res.sendFile(__dirname + "/index.jade");
+  res.render('index');
 });
 
 app.listen(8008, function() {
