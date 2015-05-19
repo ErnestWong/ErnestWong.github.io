3c2ea48738fe0e5c5acfac950847953c0369b6e3
Ernest Wong <ewong2222@hotmail.com>
2015-05-18 20:13:55 -0400

    renamed buzzwords

--- js/script.js
+++ js/script.js
@@ -39,11 +39,11 @@ $(document).ready(function() {
     $(this).find(".hover-text").fadeOut(200);
   });
 
-  var compEng = "computer engineering <a href='http://uwaterloo.ca'>@uWaterloo</a>";
+  var compEng = "computer engineering student <a href='http://uwaterloo.ca'>@uWaterloo</a>";
   var boltmade = "former developer <a href='http://boltmade.com'>@Boltmade</a>";
 
   $(".typedjs").typed({
-    strings: [compEng, boltmade, "full-stack web development", "Android development", "vim enthousiast"],
+    strings: [compEng, boltmade, "full-stack web developer", "Android developer", "vim enthousiast"],
     loop: true
   });
 });
