15255775d68b137d7279d470f47612e7cd45f940
Ernest Wong <ewong2222@hotmail.com>
2015-11-05 01:59:11 -0800

    updated resume

--- js/script.js
+++ js/script.js
@@ -1,42 +1,5 @@
 $(document).ready(function() {
-  $('body').fadeIn(2000);
-  console.log("hi");
-  $('.project-carousel').slick({
-    dots: true,
-    speed: 500,
-    slidesToScroll: 1,
-    slidesToShow: 1,
-    responsive: [
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      } 
-    ]
-  });
-
+  $('.hero-btns').fadeIn(2000, function(){ $('.hero-name').fadeIn(1000); }); 
   $("a").smoothScroll();
   $("button").smoothScroll();
-
-  // $('.header').stickyNavbar({
-  //   activeClass: "active" 
-  // });
-
-  $(".icon-center").hide();
-  $(".project-img-container").hover(function() {
-    $(this).find(".project-img").foggy();
-    $(this).find(".icon-center").fadeIn(300);
-  }, function() {
-    $(this).find(".icon-center").fadeOut(300);
-    $(this).find(".project-img").foggy(false);
-  });
-
-  $(".main-btns").hover(function() {
-    $(this).find(".hover-text").fadeIn(200);
-  }, function() {
-    $(this).find(".hover-text").fadeOut(200);
-  });
-
 });
